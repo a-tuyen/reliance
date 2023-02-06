e5beb5919feb234d623934a23c0d127987a7dd03
Amanda Tuyen <atuyen26@gmail.com>
2023-02-06 13:38:14 -0800

    adds in navbar links and installs react icons

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import Link from 'next/link'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div>
+        <div>
+            <Link href='/'>
+            <h1> Reliance</h1>
+            </Link>
+            <ul>
+                <li>
+                    <Link href='/'>Home</Link>
+                </li>
+
+                <li>
+                    <Link href='/'>Products</Link>
+                </li>
+                <li>
+                    <Link href='/'>Technical Info</Link>
+                </li>
+                <li>
+                    <Link href='/'>FAQ</Link>
+                </li>
+                <li>
+                    <Link href='/'>About Us</Link>
+                </li>
+                <li>
+                    <Link href='/'>Contact Us</Link>
+                </li>
+            </ul>
+        </div>
+    </div>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -16,8 +16,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <NavBar />
-      <Hero heading='Reliance Anodes' message='A Tradition of Excellence'/>
+      <Hero heading='Reliance Anodes' message='A Tradition of Excellence' />
     </>
   )
 }
