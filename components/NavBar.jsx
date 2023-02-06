95921dbfdde3741f31a99e066cf0b462ccb1d1fb
Amanda Tuyen <atuyen26@gmail.com>
2023-02-06 15:17:56 -0800

    adjusts hero left margin and adds in state for mobile menu function

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,37 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
 
 const NavBar = () => {
-  return (
-    <div>
-        <div>
-            <Link href='/'>
-            <h1> Reliance</h1>
-            </Link>
-            <ul>
-                <li>
-                    <Link href='/'>Home</Link>
-                </li>
 
-                <li>
-                    <Link href='/'>Products</Link>
-                </li>
-                <li>
-                    <Link href='/'>Technical Info</Link>
-                </li>
-                <li>
-                    <Link href='/'>FAQ</Link>
-                </li>
-                <li>
-                    <Link href='/'>About Us</Link>
-                </li>
-                <li>
-                    <Link href='/'>Contact Us</Link>
-                </li>
-            </ul>
+    const [nav, setNav] = useState(false)
+
+    const handleNav = () => {
+        setNav(!nav);
+    };
+
+    return (
+        <div className='fixed left-0 top-0 w-full z-10 ease-in duration-300'>
+            <div className='max-w-[1240px] m-auto flex justify-between items-center p-4 text-white'>
+                <Link href='/'>
+                    <h1 className='font-bold text-4xl '> Reliance</h1>
+                </Link>
+                <ul className='hidden sm:flex'>
+                    <li className='p-4'>
+                        <Link href='/'>Home</Link>
+                    </li>
+                    <li className='p-4'>
+                        <Link href='/products'>Products</Link>
+                    </li>
+                    <li className='p-4'>
+                        <Link href='/technical info'>Technical Info</Link>
+                    </li>
+                    <li className='p-4'>
+                        <Link href='/faq'>FAQ</Link>
+                    </li>
+                    <li className='p-4'>
+                        <Link href='/about'>About Us</Link>
+                    </li>
+                    <li className='p-4'>
+                        <Link href='/contact'>Contact Us</Link>
+                    </li>
+                </ul>
+
+                {/* Mobile Button */}
+                <div className='block sm:hidden z-10'>
+                    {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+                </div>
+                {/* Mobile Menu */}
+                <div className={nav
+                    ? 'sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300'
+                    : 'sm:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300'}>
+                    <ul>
+                        <li className='p-4 text-4xl hover:text-gray-500'>
+                            <Link href='/'>Home</Link>
+                        </li>
+                        <li className='p-4 text-4xl hover:text-gray-500'>
+                            <Link href='/products'>Products</Link>
+                        </li>
+                        <li className='p-4 text-4xl hover:text-gray-500'>
+                            <Link href='/technical info'>Technical Info</Link>
+                        </li>
+                        <li className='p-4 text-4xl hover:text-gray-500'>
+                            <Link href='/faq'>FAQ</Link>
+                        </li>
+                        <li className='p-4 text-4xl hover:text-gray-500'>
+                            <Link href='/about'>About Us</Link>
+                        </li>
+                        <li className='p-4 text-4xl hover:text-gray-500'>
+                            <Link href='/contact'>Contact Us</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default NavBar
