dc620573e61a2080ab9c61d8b2f429be9832d331
Amanda Tuyen <atuyen26@gmail.com>
2023-02-09 16:00:07 -0800

    background darkens when mobile menu is open. Changing dark to light logo on scroll is still wip

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -5,9 +5,9 @@ const Hero = ({heading, message}) => {
 
         <div className='flex items-center justify-center h-screen mb-[12rem] bg-fixed bg-center bg-cover custom-img'>
             {/* Overlay */}
-            <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/30 z-[2]'/>
+            <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/10 z-[2]'/>
             <div className='p-5 text-white z-[2] mt-[-10rem]'>
-                <h2 className='text-5xl font-bold'>{heading}</h2>
+                <h2 className='text-5xl text-white'>{heading}</h2>
                 <p className='py-5 text-xl'>{message}</p>
                 <button className=' px-8 py-2 border'>Submit</button>
             </div>
--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -2,26 +2,31 @@ import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import { HiMenu } from "react-icons/hi";
-import { CgCloseO} from "react-icons/cg";
+import { CgCloseO } from "react-icons/cg";
 import Image from "next/image";
 
 const NavBar = () => {
   const [nav, setNav] = useState(false);
   const [color, setColor] = useState("transparent");
   const [textColor, setTextColor] = useState("white");
+  const [logoDark, setLogoDark] = useState(true)
 
   const handleNav = () => {
     setNav(!nav);
   };
 
+
+
   useEffect(() => {
     const changeColor = () => {
       if (window.scrollY >= 90) {
         setColor("#ffffff");
-        setTextColor('#52525b');
+        setTextColor("#52525b");
+        setLogoDark(false);
       } else {
         setColor("transparent");
         setTextColor("#d4d4d8");
+        setLogoDark(true);
       }
     };
     window.addEventListener("scroll", changeColor);
@@ -34,14 +39,26 @@ const NavBar = () => {
     >
       <div className="max-w-[1240px] m-auto flex justify-between items-center p-4 text-white">
         <Link href="/">
-          <div className="logo-img"></div>
+
+        <div className={nav ? "fixed left-0 top-0 w-full h-screen bg-black/40" : ""}></div>
+
+        <div className={logoDark ? "absolute top-5 left-5" : "sm-hidden"}>
           <Image src='https://user-images.githubusercontent.com/77664153/217392543-b5a88ff3-8546-4130-87fb-4b385a295ee6.png' width='250' height='30'  />
+          </div>
+
+          <div className={color ? "absolute top-5 left-5" : "sm-hidden"}>
+          <Image
+            src="https://user-images.githubusercontent.com/77664153/217941326-12656f81-91f2-4e81-982b-f6a1a55bfa1b.png"
+            width="250"
+            height="30"
+          />
+          </div>
 
           {/* <Image src='https://user-images.githubusercontent.com/77664153/217392544-87c17d30-7161-485c-b09e-7fa0500c7425.png' width='250' height='35' /> */}
           {/* <h1 style={{ color: `${textColor}` }} className='font-bold text-4xl '> Reliance
                     </h1> */}
         </Link>
-        
+
         <ul style={{ color: `${textColor}` }} className="hidden sm:flex">
           <li className="p-4">
             <Link href="/">Home</Link>
@@ -64,60 +81,62 @@ const NavBar = () => {
         </ul>
 
         {/* Mobile Button */}
-        <div onClick={handleNav} className="block sm:hidden z-10">
+        <div onClick={handleNav} className="absolute right-4 sm:hidden z-10">
           {nav ? (
             <CgCloseO size={70} style={{ color: `${textColor}` }} />
           ) : (
             <HiMenu size={70} style={{ color: `${textColor}` }} />
           )}
         </div>
-        
-        {/* Mobile Menu */}
-        <div
-          className={
-            nav
-              ? "med:hidden absolute top-0 left-[50%] right-0 bottom-0 flex justify-center items-center w-[50%] h-screen bg-[#0284c7] z-5 text-center ease-in duration-300"
-              : "med:hidden absolute top-0 left-[100%] right-0 bottom-0 flex justify-center items-center w-[50%] h-screen bg-[#0089B9] text-center ease-in duration-300"
-          }
-        >
-          <ul>
-            <li
-              onClick={handleNav}
-              className="p-4 text-4xl hover:text-gray-500"
-            >
-              <Link href="/">Home</Link>
-            </li>
-            <li
-              onClick={handleNav}
-              className="p-4 text-4xl hover:text-gray-500"
-            >
-              <Link href="/products">Products</Link>
-            </li>
-            <li
-              onClick={handleNav}
-              className="p-4 text-4xl hover:text-gray-500"
-            >
-              <Link href="/technical info">Technical Info</Link>
-            </li>
-            <li
-              onClick={handleNav}
-              className="p-4 text-4xl hover:text-gray-500"
-            >
-              <Link href="/faq">FAQ</Link>
-            </li>
-            <li
-              onClick={handleNav}
-              className="p-4 text-4xl hover:text-gray-500"
-            >
-              <Link href="/about">About Us</Link>
-            </li>
-            <li
-              onClick={handleNav}
-              className="p-4 text-4xl hover:text-gray-500"
-            >
-              <Link href="/contact">Contact Us</Link>
-            </li>
-          </ul>
+
+        <div className={nav ? "fixed left-0 top-0 w-full h-screen bg-black/40" : ""}>
+          {/* Mobile Menu */}
+          <div
+            className={
+              nav
+                ? "med:hidden absolute top-0 left-[50%] right-0 bottom-0 flex justify-center items-center w-[50%] h-screen bg-[#5c90d2] z-5 text-center ease-in duration-300"
+                : "med:hidden absolute top-0 left-[100%] right-0 bottom-0 flex justify-center items-center w-[50%] h-screen bg-[#5c90d2] text-center ease-in duration-300"
+            }
+          >
+            <ul>
+              <li
+                onClick={handleNav}
+                className="p-4 text-4xl hover:text-gray-500"
+              >
+                <Link href="/">Home</Link>
+              </li>
+              <li
+                onClick={handleNav}
+                className="p-4 text-4xl hover:text-gray-500"
+              >
+                <Link href="/products">Products</Link>
+              </li>
+              <li
+                onClick={handleNav}
+                className="p-4 text-4xl hover:text-gray-500"
+              >
+                <Link href="/technical info">Technical Info</Link>
+              </li>
+              <li
+                onClick={handleNav}
+                className="p-4 text-4xl hover:text-gray-500"
+              >
+                <Link href="/faq">FAQ</Link>
+              </li>
+              <li
+                onClick={handleNav}
+                className="p-4 text-4xl hover:text-gray-500"
+              >
+                <Link href="/about">About Us</Link>
+              </li>
+              <li
+                onClick={handleNav}
+                className="p-4 text-4xl hover:text-gray-500"
+              >
+                <Link href="/contact">Contact Us</Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
