d5f7d0a615787a01fcce3931bdf09014cf242eb1
Amanda Tuyen <atuyen26@gmail.com>
2023-02-10 13:47:23 -0800

    changing logo colours is kinda working, has dupe underneath

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -9,25 +9,25 @@ const NavBar = () => {
   const [nav, setNav] = useState(false);
   const [color, setColor] = useState("transparent");
   const [textColor, setTextColor] = useState("white");
-  const [logoDark, setLogoDark] = useState(true)
+  const [logoDark, setLogoDark] = useState(true);
 
   const handleNav = () => {
     setNav(!nav);
   };
-
-
 
   useEffect(() => {
     const changeColor = () => {
       if (window.scrollY >= 90) {
         setColor("#ffffff");
         setTextColor("#52525b");
-        setLogoDark(false);
+        setLogoDark(true);
+
       } else {
         setColor("transparent");
         setTextColor("#d4d4d8");
-        setLogoDark(true);
+        setLogoDark(false);
       }
+      return console.log(logoDark);
     };
     window.addEventListener("scroll", changeColor);
   }, []);
@@ -35,28 +35,28 @@ const NavBar = () => {
   return (
     <div
       style={{ backgroundColor: `${color}` }}
-      className="fixed left-0 top-0 w-full z-10 ease-in duration-300"
+      className="fixed left-0 top-0 w-full z-10 ease-in duration-300 block"
     >
       <div className="max-w-[1240px] m-auto flex justify-between items-center p-4 text-white">
         <Link href="/">
 
-        <div className={nav ? "fixed left-0 top-0 w-full h-screen bg-black/40" : ""}></div>
-
-        <div className={logoDark ? "absolute top-5 left-5" : "sm-hidden"}>
-          <Image src='https://user-images.githubusercontent.com/77664153/217392543-b5a88ff3-8546-4130-87fb-4b385a295ee6.png' width='250' height='30'  />
+          <div className={logoDark ? "absolute top-1.5 left-3.5" : " sm-hidden"}>
+            <Image
+              src="https://user-images.githubusercontent.com/77664153/217392543-b5a88ff3-8546-4130-87fb-4b385a295ee6.png"
+              width="200"
+              height="30"
+              
+              />
           </div>
 
-          <div className={color ? "absolute top-5 left-5" : "sm-hidden"}>
-          <Image
-            src="https://user-images.githubusercontent.com/77664153/217941326-12656f81-91f2-4e81-982b-f6a1a55bfa1b.png"
-            width="250"
-            height="30"
-          />
+          <div className={logoDark ? "sm-hidden" : "absolute top-3.5 left-3.1"}>
+            <Image
+              src="https://user-images.githubusercontent.com/77664153/217941326-12656f81-91f2-4e81-982b-f6a1a55bfa1b.png"
+              width="200"
+              height="30"
+            />
           </div>
 
-          {/* <Image src='https://user-images.githubusercontent.com/77664153/217392544-87c17d30-7161-485c-b09e-7fa0500c7425.png' width='250' height='35' /> */}
-          {/* <h1 style={{ color: `${textColor}` }} className='font-bold text-4xl '> Reliance
-                    </h1> */}
         </Link>
 
         <ul style={{ color: `${textColor}` }} className="hidden sm:flex">
@@ -81,7 +81,10 @@ const NavBar = () => {
         </ul>
 
         {/* Mobile Button */}
-        <div onClick={handleNav} className="absolute right-4 sm:hidden z-10">
+        <div
+          onClick={handleNav}
+          className="absolute right-4 top-10 sm:hidden z-10"
+        >
           {nav ? (
             <CgCloseO size={70} style={{ color: `${textColor}` }} />
           ) : (
@@ -89,7 +92,11 @@ const NavBar = () => {
           )}
         </div>
 
-        <div className={nav ? "fixed left-0 top-0 w-full h-screen bg-black/40" : ""}>
+        <div
+          className={
+            nav ? "fixed left-0 top-0 w-full h-screen bg-black/40" : ""
+          }
+        >
           {/* Mobile Menu */}
           <div
             className={
