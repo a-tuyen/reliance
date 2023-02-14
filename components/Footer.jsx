3791529c2cb6dd7658ef8859bffa130fb2566318
Amanda Tuyen <atuyen26@gmail.com>
2023-02-14 15:30:25 -0800

    footer is mostly formatted

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { GiRotaryPhone } from "react-icons/gi";
-import { SiOpenstreetmap } from "react-icons/si";
 import { TbMap2 } from "react-icons/tb";
 import { IoIosMail } from "react-icons/io";
 
@@ -8,21 +7,20 @@ import { IoIosMail } from "react-icons/io";
 const Footer = () => {
   return (
     <div className="bg-[#4C4E52]">
-    <div className="bg-black/[40%] h-[17rem] w-[20rem] pl-[1rem] ">
-      <ul className="text-white">
-        <h2>WE ARE HERE TO HELP</h2>
-        <li className="flex items-center">
+    <div className="bg-black/[30%] h-[17rem] w-[25rem] pl-[1rem] ml-[2rem]  ">
+      <ul className="text-white text-xl ">
+        <h2 className="text-3xl pt-8 pb-3">WE ARE HERE TO HELP</h2>
+        <li className="flex items-center p-2">
           <GiRotaryPhone size={30}/> <p className="pl-2">1 (877) 420 3731</p>
         </li>
-        <li className="flex items-center">
+        <li className="flex items-center p-2">
           <IoIosMail size={30} /><p className="pl-2">info@metaldist.com</p>
         </li>
-        <li className="flex items-center">
-            <TbMap2 size={30} />
-          <p className="pl-2">7220 Winston Street, Burnaby, BC V5A 2G9</p>
+        <li className="flex items-center p-2">
+            <TbMap2 size={30} /><p className="pl-2">7220 Winston Street, Burnaby, BC V5A 2G9</p>
         </li>
         <li>
-          <p>Copyright © 2022 Metal Distributors LTD</p>
+          <p className="pt-2">Copyright © 2022 Metal Distributors LTD</p>
         </li>
       </ul>
     </div>
