bb1531d6603bb4e70eff306406e54f2ee48b993d
Amanda Tuyen <atuyen26@gmail.com>
2023-02-14 12:23:42 -0800

    adds in Footer

---
+++ components/Footer.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <ul>
+        <h2>WE ARE HERE TO HELP</h2>
+        <li>
+          <p>1 (877) 420 3731</p>
+        </li>
+        <li>
+          <p>info@metaldist.com</p>
+        </li>
+        <li>
+          <p>7220 Winston Street, Burnaby, BC V5A 2G9</p>
+        </li>
+        <li>
+          <p>Copyright © 2022 Metal Distributors LTD</p>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Footer;
