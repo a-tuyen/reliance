298b0539d2b09f9bd8c6dcb04c161ed6fe10c8c0
Amanda Tuyen <atuyen26@gmail.com>
2023-02-14 15:08:28 -0800

    adds in footer icons

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,23 +1,31 @@
 import React from "react";
+import { GiRotaryPhone } from "react-icons/gi";
+import { SiOpenstreetmap } from "react-icons/si";
+import { TbMap2 } from "react-icons/tb";
+import { IoIosMail } from "react-icons/io";
+
 
 const Footer = () => {
   return (
-    <div>
-      <ul>
+    <div className="bg-[#4C4E52]">
+    <div className="bg-black/[40%] h-[17rem] w-[20rem] pl-[1rem] ">
+      <ul className="text-white">
         <h2>WE ARE HERE TO HELP</h2>
-        <li>
-          <p>1 (877) 420 3731</p>
+        <li className="flex items-center">
+          <GiRotaryPhone size={30}/> <p className="pl-2">1 (877) 420 3731</p>
         </li>
-        <li>
-          <p>info@metaldist.com</p>
+        <li className="flex items-center">
+          <IoIosMail size={30} /><p className="pl-2">info@metaldist.com</p>
         </li>
-        <li>
-          <p>7220 Winston Street, Burnaby, BC V5A 2G9</p>
+        <li className="flex items-center">
+            <TbMap2 size={30} />
+          <p className="pl-2">7220 Winston Street, Burnaby, BC V5A 2G9</p>
         </li>
         <li>
           <p>Copyright © 2022 Metal Distributors LTD</p>
         </li>
       </ul>
+    </div>
     </div>
   );
 };
