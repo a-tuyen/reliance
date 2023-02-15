aa6648b2f4e32cbbf95aa1b8dad1637affa1da98
Amanda Tuyen <atuyen26@gmail.com>
2023-02-15 13:46:58 -0800

    adds in new fonts and implements them

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import { GiRotaryPhone } from "react-icons/gi";
 import { TbMap2 } from "react-icons/tb";
 import { IoIosMail } from "react-icons/io";
+import { GrMapLocation } from "react-icons/gr";
 
 
 const Footer = () => {
   return (
     <div className="bg-[#4C4E52]">
-    <div className="bg-black/[30%] h-[17rem] w-[100%] md:w-[60%] lg:w-[40%] pl-[6%] ml-[0] md:ml-[4%] ">
+    <div className="bg-[#393b3e] h-[17rem] w-[100%] md:w-[60%] lg:w-[35%] pl-[2%] ml-[0] md:ml-[4%] flex justify-center">
       <ul className="text-white text-xl ">
-        <h2 className="text-3xl pt-6 pb-3">WE ARE HERE TO HELP</h2>
+        <h2 className="text-4xl pt-6 pb-3 font-heading tracking-wide">WE ARE HERE TO HELP</h2>
         <li className="flex items-center p-2">
           <GiRotaryPhone size={30}/> <p className="pl-2">1 (877) 420 3731</p>
         </li>
--- components/Intro.jsx
+++ components/Intro.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Intro = () => {
   return (
-    <div>Intro</div>
+    <div className='bg-white'>
+<p>​​Metal Distributors is a leading non-ferrous metal manufacturer and precision machining company serving a variety of different industries including the marine, nuclear, plumbing, construction and medical industries.</p>
+        <p>We specialize in lead and lead-based products such as Sheet Lead and Lead Bricks for radiation shielding, Lead Anodes for electroplating and Lead Sash Weights for counter balances. We also offer our Reliance brand of sacrificial anodes​ and custom die casting and CNC services. View our full lineup of products <a href='/products'>here.</a></p>
+        <p>Our technicians are knowledgeable in all aspects of metalwork, from design and pouring to metal finishing and coating. We can help you find a solution to your metal needs.</p>
+
+    </div>
   )
 }
 
