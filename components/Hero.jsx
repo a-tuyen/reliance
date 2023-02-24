f36a2ed7b5d94f25b5fb9472e64d1061e7e1430f
Amanda Tuyen <atuyen26@gmail.com>
2023-02-24 14:43:12 -0800

    using Image now works for hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -9,27 +9,28 @@ import bg4 from '../public/backgrounds/relianceBg4.jpeg'
 const Hero = ({ heading, message }) => {
   const imgArr = ["img", "img2", "img3", "img4"];
 
-  const picSelector = () => {
+  const picSelector = bg2; 
 
-    return bg2;
+  // const picSelector = () => {
+
+  //   return bg2;
   
-  };
+  // };
 
 
   return (
     <div className="h-screen">
-      <div className="fixed z-5">
-        <Image src={picSelector} width={2400} height={1000} objectFit="cover" quality={100} />
+      <div className="static">
+        <Image src={picSelector} fill objectFit="cover" quality={100} />
       </div>
-      <div classname="bg-white" > Some overlay things go in here </div>
+      <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/30 z-[1]'/>
+        <div className='flex flex-col items-center p-5 text-white z-[2] mt-[10rem]'>
+            <h2 className='text-6xl text-white font-boldHeading tracking-wider text-center z-40'>{heading}</h2>
+            <p className='flex justify-center w-[70%] py-10 text-3xl text-white text-center font-thin z-40'>{message}</p>
+        </div>
     </div>
     // <div className='flex items-center justify-center h-screen mb-0 bg-fixed bg-center bg-cover bg-img1'>
-    //     {/* Overlay */}
-    //     <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/30 z-[1]'/>
-    //     <div className='flex flex-col items-center p-5 text-white z-[2] mt-[10rem]'>
-    //         <h2 className='text-6xl text-white font-boldHeading tracking-wider text-center'>{heading}</h2>
-    //         <p className='flex justify-center w-[70%] py-10 text-3xl text-white text-center font-thin'>{message}</p>
-    //     </div>
+        // {/* Overlay */}
     // </div>
   );
 };
