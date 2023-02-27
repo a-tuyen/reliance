29f5f527a3de663398a3aa58bd03aaacb353dc63
Amanda Tuyen <atuyen26@gmail.com>
2023-02-27 11:24:37 -0800

    removes image for backgrounds for faq page goes back to css way

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -8,9 +8,10 @@ import bg4 from "../public/backgrounds/relianceBg4.jpeg";
 const Hero = ({ heading, message, img }) => {
   return (
     <div className="h-[100%] pb-0 mb-0">
-      <div className="static">
+    <div className={`flex items-center justify-center h-screen mb-0 bg-fixed bg-center bg-cover ${img}`}></div>
+      {/* <div className="static">
         <Image src={img} alt="" priority fill objectFit="cover" quality={100} />
-      </div>
+      </div> */}
       <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/30 z-[1]" />
       <div className="flex flex-col items-center p-5 text-white z-[1] mt-[10rem]">
         <h2 className="text-6xl text-white font-boldHeading tracking-wider text-center mt-[13%] z-[1]">
