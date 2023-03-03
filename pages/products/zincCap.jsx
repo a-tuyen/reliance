f60b3aed193c46f340a309436c58f047c32a65f9
Amanda Tuyen <atuyen26@gmail.com>
2023-03-03 14:07:43 -0800

    finished zinc cap dimension chart

--- pages/products/zincCap.jsx
+++ pages/products/zincCap.jsx
@@ -59,173 +59,136 @@ const zincCap = () => {
         </tbody>
       </table>
 
-      <h1>Reliance Zinc Cap - 6 oz</h1>
-      <table>
+      {/* <h1>Reliance Zinc Cap - 6 oz</h1> */}
+      <table className="bg-gray-300 mx-[10%]">
         <tbody>
-        <tr>
-          <th colSpan={"7"}>Dimensions</th>
-          <th colSpan={"2"}>Nominal Weight</th>
+        <tr className="bg-black text-white">
+          <th colSpan={"9"} className="border-2 border-white py-4"><h1>Reliance Zinc Cap Specifications</h1></th>
         </tr>
-        <tr>
+        <tr className="uppercase bg-black text-white">
+        <th className="border-2 border-white">Thread Size</th>
+          <th colSpan={"6"} className="border-2 border-white">Dimensions</th>
+          <th colSpan={"2"} className="border-2 border-white">Nominal Weight</th>
+        </tr>
+        <tr className="uppercase bg-black text-white">
           <th>A</th>
-          <th colSpan={"2"}>B</th>
-          <th colSpan={"2"}>C</th>
-          <th colSpan={"2"}>D</th>
-          <th colSpan={"2"}></th>
+          <th colSpan={"2"} className="border-2 border-white">B</th>
+          <th colSpan={"2"} className="border-2 border-white">C</th>
+          <th colSpan={"2"} className="border-2 border-white">D</th>
+          <th colSpan={"2"} className="border-2 border-white"></th>
         </tr>
         <tr>
-          <th>THREAD SIZE</th>
-          <th>INCHES</th>
-          <th>mm</th>
-          <th>INCHES</th>
-          <th>mm</th>
-          <th>INCHES</th>
-          <th>mm</th>
-          <th>lbs</th>
-          <th>kg</th>
+          <th className="border-2 border-white text-center">Reliance Zinc Cap - 6 oz</th>
+          <th className="border-2 border-white border-r-0 text-center">Inches</th>
+          <th className="border-2 border-white border-l-0 text-center">mm</th>
+          <th className="border-2 border-white border-r-0 text-center">Inches</th>
+          <th className="border-2 border-white border-l-0 text-center">mm</th>
+          <th className="border-2 border-white border-r-0 text-center">Inches</th>
+          <th className="border-2 border-white border-l-0 text-center">mm</th>
+          <th className="border-2 border-white border-r-0 text-center">lbs</th>
+          <th className="border-2 border-white border-l-0 text-center">kg</th>
         </tr>
         <tr>
-          <td>1/2&quot; x 13 UNC</td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
+          <td className="border-2 border-white text-center">1/2&quot; x 13 UNC</td>
+          <td className="border-2 border-white border-r-0 text-center">1.5</td>
+          <td className="border-2 border-white border-l-0 text-center">38.1</td>
+          <td className="border-2 border-white border-r-0 text-center">0.98</td>
+          <td className="border-2 border-white border-l-0 text-center">24.9</td>
+          <td className="border-2 border-white border-r-0 text-center">1.69</td>
+          <td className="border-2 border-white border-l-0 text-center">42.95</td>
+          <td className="border-2 border-white border-r-0 text-center">0.4</td>
+          <td className="border-2 border-white border-l-0 text-center">0.15</td>
         </tr>
         <tr>
-          <td>5/8&quot; x 11 UNC</td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
+          <td className="border-2 border-white text-center">5/8&quot; x 11 UNC</td>
+          <td className="border-2 border-white border-r-0 text-center">1.5</td>
+          <td className="border-2 border-white border-l-0 text-center">38.1</td>
+          <td className="border-2 border-white border-r-0 text-center">0.98</td>
+          <td className="border-2 border-white border-l-0 text-center">24.9</td>
+          <td className="border-2 border-white border-r-0 text-center">1.69</td>
+          <td className="border-2 border-white border-l-0 text-center">42.95</td>
+          <td className="border-2 border-white border-r-0 text-center">0.4</td>
+          <td className="border-2 border-white border-l-0 text-center">0.15</td>
         </tr>
         <tr>
-          <td>3/4&quot; x 10 UNC</td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
+          <td className="border-2 border-white text-center">3/4&quot; x 10 UNC</td>
+          <td className="border-2 border-white border-r-0 text-center">1.5</td>
+          <td className="border-2 border-white border-l-0 text-center">38.1</td>
+          <td className="border-2 border-white border-r-0 text-center">0.98</td>
+          <td className="border-2 border-white border-l-0 text-center">24.9</td>
+          <td className="border-2 border-white border-r-0 text-center">1.69</td>
+          <td className="border-2 border-white border-l-0 text-center">42.95</td>
+          <td className="border-2 border-white border-r-0 text-center">0.4</td>
+          <td className="border-2 border-white border-l-0 text-center">0.15</td>
         </tr>
         <tr>
-          <td>7/8&quot; x 9 UNC</td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
+          <td className="border-2 border-white text-center">7/8&quot; x 9 UNC</td>
+          <td className="border-2 border-white border-r-0 text-center">1.5</td>
+          <td className="border-2 border-white border-l-0 text-center">38.1</td>
+          <td className="border-2 border-white border-r-0 text-center">0.98</td>
+          <td className="border-2 border-white border-l-0 text-center">24.9</td>
+          <td className="border-2 border-white border-r-0 text-center">1.69</td>
+          <td className="border-2 border-white border-l-0 text-center">42.95</td>
+          <td className="border-2 border-white border-r-0 text-center">0.4</td>
+          <td className="border-2 border-white border-l-0 text-center">0.15</td>
         </tr>
         <tr>
-          <td>1&quot; x 8 UNC</td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
+          <td className="border-2 border-white text-center">1&quot; x 8 UNC</td>
+          <td className="border-2 border-white border-r-0 text-center">1.5</td>
+          <td className="border-2 border-white border-l-0 text-center">38.1</td>
+          <td className="border-2 border-white border-r-0 text-center">0.98</td>
+          <td className="border-2 border-white border-l-0 text-center">24.9</td>
+          <td className="border-2 border-white border-r-0 text-center">1.69</td>
+          <td className="border-2 border-white border-l-0 text-center">42.95</td>
+          <td className="border-2 border-white border-r-0 text-center">0.4</td>
+          <td className="border-2 border-white border-l-0 text-center">0.15</td>
+        </tr>
+        <tr>
+          <th className="border-2 border-white text-center">Reliance Zinc Cap - 14 oz</th>
+          <th className="border-2 border-white border-r-0 text-center">Inches</th>
+          <th className="border-2 border-white border-l-0 text-center">mm</th>
+          <th className="border-2 border-white border-r-0 text-center">Inches</th>
+          <th className="border-2 border-white border-l-0 text-center">mm</th>
+          <th className="border-2 border-white border-r-0 text-center">Inches</th>
+          <th className="border-2 border-white border-l-0 text-center">mm</th>
+          <th className="border-2 border-white border-r-0 text-center">lbs</th>
+          <th className="border-2 border-white border-l-0 text-center">kg</th>
+        </tr>
+        <tr>
+          <td className="border-2 border-white text-center">1 1/8&quot; x 7 UNC</td>
+          <td className="border-2 border-white border-r-0 text-center">2.3</td>
+          <td className="border-2 border-white border-l-0 text-center">58.4</td>
+          <td className="border-2 border-white border-r-0 text-center">1.25</td>
+          <td className="border-2 border-white border-l-0 text-center">31.8</td>
+          <td className="border-2 border-white border-r-0 text-center">1.88</td>
+          <td className="border-2 border-white border-l-0 text-center">47.75</td>
+          <td className="border-2 border-white border-r-0 text-center">0.9</td>
+          <td className="border-2 border-white border-l-0 text-center">0.4</td>
+        </tr>
+        <tr>
+          <td className="border-2 border-white text-center">1 1/4&quot; x 7 UNC</td>
+          <td className="border-2 border-white border-r-0 text-center">2.3</td>
+          <td className="border-2 border-white border-l-0 text-center">58.4</td>
+          <td className="border-2 border-white border-r-0 text-center">1.25</td>
+          <td className="border-2 border-white border-l-0 text-center">31.8</td>
+          <td className="border-2 border-white border-r-0 text-center">1.88</td>
+          <td className="border-2 border-white border-l-0 text-center">47.75</td>
+          <td className="border-2 border-white border-r-0 text-center">0.9</td>
+          <td className="border-2 border-white border-l-0 text-center">0.4</td>
+        </tr>
+        <tr>
+          <td className="border-2 border-white text-center">1 1/2&quot; x 7 UNC</td>
+          <td className="border-2 border-white border-r-0 text-center">2.3</td>
+          <td className="border-2 border-white border-l-0 text-center">58.4</td>
+          <td className="border-2 border-white border-r-0 text-center">1.25</td>
+          <td className="border-2 border-white border-l-0 text-center">31.8</td>
+          <td className="border-2 border-white border-r-0 text-center">1.88</td>
+          <td className="border-2 border-white border-l-0 text-center">47.75</td>
+          <td className="border-2 border-white border-r-0 text-center">0.7</td>
+          <td className="border-2 border-white border-l-0 text-center">0.3</td>
         </tr>
         </tbody>
       </table>
-
-      <h1>Reliance Zinc Cap - 6 oz</h1>
-      <table>
-        <tbody>
-        <tr>
-          <th colSpan={"7"}>Dimensions</th>
-          <th colSpan={"2"}>Nominal Weight</th>
-        </tr>
-        <tr>
-          <th>A</th>
-          <th colSpan={"2"}>B</th>
-          <th colSpan={"2"}>C</th>
-          <th colSpan={"2"}>D</th>
-          <th colSpan={"2"}></th>
-        </tr>
-        <tr>
-          <th>THREAD SIZE</th>
-          <th>INCHES</th>
-          <th>mm</th>
-          <th>INCHES</th>
-          <th>mm</th>
-          <th>INCHES</th>
-          <th>mm</th>
-          <th>lbs</th>
-          <th>kg</th>
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        </tbody>
-      </table>
-
-
     </div>
   );
 };
