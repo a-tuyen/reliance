cd111587a7205c94af4626272fedec177b27017a
Amanda Tuyen <atuyen26@gmail.com>
2023-03-10 14:42:19 -0800

    adds in crab trap chart

--- pages/products/commercial/crabTrap.jsx
+++ pages/products/commercial/crabTrap.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import Hero from "@/components/Hero";
+import SideBarComm from "@/components/SideBarComm";
 
 const crabTrap = () => {
   return (
-    <div>crabTrap</div>
-  )
-}
+    <div>
+      <Hero heading={"Crab Trap Anodes"} message={""} img={"bg-img5"} />
+      <div className="flex flex-col lg:flex-row items-center lg:justify-start w-[100%]">
+        <SideBarComm />
+        <main>
+          <article>
+            <table>
+              <tbody>
+                <tr>
+                  <th></th>
+                </tr>
+              </tbody>
+            </table>
+          </article>
+        </main>
+      </div>
+    </div>
+  );
+};
 
-export default crabTrap
+export default crabTrap;
