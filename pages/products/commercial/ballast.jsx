3d63faf1a4f057463b425b3cfddf4f4dec5aa6c6
Amanda Tuyen <atuyen26@gmail.com>
2023-03-13 14:06:37 -0700

    adds in ballast chart

--- pages/products/commercial/ballast.jsx
+++ pages/products/commercial/ballast.jsx
@@ -1,8 +1,89 @@
 import React from 'react'
+import Hero from '@/components/Hero'
+import SideBarComm from '@/components/SideBarComm'
+import Chart3Dim from '@/components/Chart3Dim'
+
+const ballastData = [
+  {
+    znCode: 'ZMD3602',
+    znLb: '90.0',
+    znKg: '(40.91)',
+    alCode: 'AMD3602',
+    alLb: '35',
+    alKg: '(16)',
+    diaIn: '60.0',
+    diaMm: '(1524)',
+    widIn: '2.5',
+    widMm: '64',
+    cDiaIn: '3.0',
+    cDiaMm: '(76)',
+
+  }
+]
 
 const ballast = () => {
   return (
-    <article className='bg-white'>ballast</article>
+    <div>
+    <Hero heading={"Ballast Tank Anodes"} message={""} img={"bg-img5"} />
+    <div className="flex flex-col lg:flex-row items-center lg:justify-start w-[100%]">
+<SideBarComm />
+<article>
+<main>
+  <table>
+    <tbody>
+      <tr>
+        <th colSpan={6}>NOMINAL WEIGHT</th>
+        <th colSpan={6}>NOMINAL DIMENSIONS</th>
+      </tr>
+      <tr>
+        <th colSpan={3} className="bg-zinc">
+          ZINC ANODE
+        </th>
+        <th colSpan={3} className="bg-alum">
+          ALUMINUM ANODE
+        </th>
+        <th colSpan={2}>DIAMETER</th>
+        <th colSpan={2}>WIDTH</th>
+        <th colSpan={2}>BOLT DIAMETER</th>
+      </tr>
+
+      <tr>
+        <th className="bg-zinc">ITEM #</th>
+        <th className="bg-zinc border-r-0">lb</th>
+        <th className="bg-zinc border-l-0">(kg)</th>
+        <th className="bg-alum">ITEM #</th>
+        <th className="bg-alum border-r-0">lb</th>
+        <th className="bg-alum border-l-0">(kg)</th>
+        <th className="border-r-0">in</th>
+        <th className="border-l-0">(mm)</th>
+        <th className="border-r-0">in</th>
+        <th className="border-l-0">(mm)</th>
+        <th className="border-r-0">in</th>
+        <th className="border-l-0">(mm)</th>
+      </tr>
+      {ballastData.map((item, id) => (
+        <Chart3Dim
+          key={id}
+          znCode={item.znCode}
+          znLb={item.znLb}
+          znKg={item.znKg}
+          alCode={item.alCode}
+          alLb={item.alLb}
+          alKg={item.alKg}
+          diaIn={item.diaIn}
+          diaMm={item.diaMm}
+          widIn={item.widIn}
+          widMm={item.widMm}
+          cDiaIn={item.cDiaIn}
+          cDiaMm={item.cDiaMm}
+        />
+      ))}
+    </tbody>
+  </table>
+</main>
+</article>
+</div>
+</div>
   )
 }
 
