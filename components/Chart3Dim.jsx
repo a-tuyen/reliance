f190d810e6642c1be3046df3452149fdea797df2
Amanda Tuyen <atuyen26@gmail.com>
2023-03-23 15:54:38 -0700

    converts bolt on chart to stacked measurements

--- components/Chart3Dim.jsx
+++ components/Chart3Dim.jsx
@@ -15,22 +15,29 @@ const Chart3Dim = ({
   cDiaMm,
 }) => {
   return (
+    <>
+      <tr>
+        <td className="bg-zinc text-white border-l-0 px-1">ZN</td>
+        <td className="bg-zincLight">{znCode}</td>
+        <td className="bg-zincLight border-r-0">
+          {znLb} {znKg}
+        </td>
+        <td className="border-b-0">{diaIn}</td>
+        <td className="border-b-0">{widIn}</td>
+        <td className="border-b-0">{cDiaIn}</td>
+      </tr>
 
-        <tr>
-          <td className="bg-zincLight">{znCode}</td>
-          <td className="bg-zincLight border-r-0">{znLb}</td>
-          <td className="bg-zincLight border-l-0">{znKg}</td>
-          <td className="bg-alumLight">{alCode}</td>
-          <td className="bg-alumLight border-r-0">{alLb}</td>
-          <td className="bg-alumLight border-l-0">{alKg}</td>
-          <td className="border-r-0">{diaIn}</td>
-          <td className="border-l-0">{diaMm}</td>
-          <td className="border-r-0">{widIn}</td>
-          <td className="border-l-0">{widMm}</td>
-          <td className="border-r-0">{cDiaIn}</td>
-          <td className="border-l-0">{cDiaMm}</td>
-        </tr>
-
+      <tr>
+        <td className="bg-alum text-white border-l-0">AL</td>
+        <td className="bg-alumLight">{alCode}</td>
+        <td className="bg-alumLight border-r-0">
+          {alLb} {alKg}
+        </td>
+        <td className="border-t-0">{diaMm}</td>
+        <td className="border-t-0">{widMm}</td>
+        <td className="border-t-0">{cDiaMm}</td>
+      </tr>
+    </>
   );
 };
 
--- pages/products/commercial/boltOn.jsx
+++ pages/products/commercial/boltOn.jsx
@@ -64,39 +64,21 @@ const boltOn = () => {
             <table>
               <tbody>
                 <tr>
-                  <th colSpan={12} className="py-8">
+                  <th colSpan={6} className="py-8">
                     <h1>BOLT-ON (PIE ANODE) SPECIFICATIONS</h1>
                   </th>
                 </tr>
                 <tr>
-                  <th colSpan={6}>NOMINAL WEIGHT</th>
-                  <th colSpan={6}>NOMINAL DIMENSIONS</th>
-                </tr>
-                <tr>
-                  <th colSpan={3} className="bg-zinc">
-                    ZINC ANODE
-                  </th>
-                  <th colSpan={3} className="bg-alum">
-                    ALUMINUM ANODE
-                  </th>
-                  <th colSpan={2}>DIAMETER</th>
-                  <th colSpan={2}>WIDTH</th>
-                  <th colSpan={2}>BOLT DIAMETER</th>
+                  <th colSpan={3}>NOMINAL WEIGHT</th>
+                  <th colSpan={3}>NOMINAL DIMENSIONS</th>
                 </tr>
 
                 <tr>
-                  <th className="bg-zinc">ITEM #</th>
-                  <th className="bg-zinc border-r-0">lb</th>
-                  <th className="bg-zinc border-l-0">(kg)</th>
-                  <th className="bg-alum">ITEM #</th>
-                  <th className="bg-alum border-r-0">lb</th>
-                  <th className="bg-alum border-l-0">(kg)</th>
-                  <th className="border-r-0">in</th>
-                  <th className="border-l-0">(mm)</th>
-                  <th className="border-r-0">in</th>
-                  <th className="border-l-0">(mm)</th>
-                  <th className="border-r-0">in</th>
-                  <th className="border-l-0">(mm)</th>
+                  <th colspan={2} className="">ITEM #</th>
+                  <th>lb  (kg)</th>
+                  <th>DIAMETER</th>
+                  <th>WIDTH</th>
+                  <th>BOLT DIAMETER</th>
                 </tr>
                 {boltOnData.map((item, id) => (
                   <Chart3Dim
