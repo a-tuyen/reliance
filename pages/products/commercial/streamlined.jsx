b602626ace6f46ed02ee1757a64cf80ca1def3ab
Amanda Tuyen <atuyen26@gmail.com>
2023-03-24 11:25:49 -0700

    updates streamlined pics

--- pages/products/commercial/streamlined.jsx
+++ pages/products/commercial/streamlined.jsx
@@ -4,52 +4,60 @@ import SideBarComm from "@/components/SideBarComm";
 import Chart4DimPic from "@/components/Chart4DimPic";
 
 const streamlinedData = [
-  {img: 'https://user-images.githubusercontent.com/77664153/227369245-9901a9b2-8911-4b3c-bdaf-63df48ab93b9.png',
-  znCode: 'MD-Z-22',
-  znLb: '23.0 lb',
-  znKg: '(10.43 kg)',
-  alCode: 'AMD-Z-22',
-  alLb: '10.0 lb',
-  alKg: '(4.54 kg)',
-  diaIn: '14.0"',
-  diaMm: '(356 mm)',
-  widIn: '6.5"',
-  widMm: '(165 mm)',
-  cDiaIn: '1 1/2"',
-  cDiaMm: '(38 mm)',
-  dMeasureIn: '-',
-  dMeasureMm: '-',
-  notes: 'AVAILABLE WITH STEEL OR ALUMINUM STRAPS',},
-  
+  {
+    img: "https://user-images.githubusercontent.com/77664153/227608602-227604fe-0679-4e20-9819-a87f74068e19.png",
+    znCode: "MD-Z-22",
+    znLb: "23.0 lb",
+    znKg: "(10.43 kg)",
+    alCode: "AMD-Z-22",
+    alLb: "10.0 lb",
+    alKg: "(4.54 kg)",
+    diaIn: '14.0"',
+    diaMm: "(356 mm)",
+    widIn: '6.5"',
+    widMm: "(165 mm)",
+    cDiaIn: '1 1/2"',
+    cDiaMm: "(38 mm)",
+    dMeasureIn: '-"',
+    dMeasureMm: "(- mm)",
+    notes: "AVAILABLE WITH STEEL OR ALUMINUM STRAPS",
+  },
 
-  {img: 'https://user-images.githubusercontent.com/77664153/227366105-8cee6b43-336c-46a2-b781-bcc9ac3ccd16.png',
-  znCode: 'MD-Z-26 / MD-ZHS-26',
-  znLb: '25 lb',
-  znKg: '(11.34 kg)',
-  alCode: 'AMD-Z-26 / AMD-ZHS-26',
-  alLb: '12 lb',
-  alKg: '(5.44 kg)',
-  diaIn: '13.25"',
-  diaMm: '(337 mm)',
-  widIn: '4.25"',
-  widMm: '(108 mm)',
-  cDiaIn: '2 1/4"',
-  cDiaMm: '(57 mm)',
-  dMeasureIn: '-',
-  dMeasureMm: '-',
-  notes: 'AVAILABLE WITH STEEL OR ALUMINUM STRAPS',},
-  
+  {
+    img: "https://user-images.githubusercontent.com/77664153/227608605-1e4fd659-4e3e-457d-bfa4-519e1a8b89c0.png",
+    znCode: "MD-Z-26 / MD-ZHS-26",
+    znLb: "25 lb",
+    znKg: "(11.34 kg)",
+    alCode: "AMD-Z-26 / AMD-ZHS-26",
+    alLb: "12 lb",
+    alKg: "(5.44 kg)",
+    diaIn: '13.25"',
+    diaMm: "(337 mm)",
+    widIn: '4.25"',
+    widMm: "(108 mm)",
+    cDiaIn: '2 1/4"',
+    cDiaMm: "(57 mm)",
+    dMeasureIn: '-"',
+    dMeasureMm: "(- mm)",
+    notes: "AVAILABLE WITH STEEL OR ALUMINUM STRAPS",
+  },
 ];
 
 const streamlined = () => {
   return (
     <div>
-      <Hero heading={"streamlined Anodes"} message={"Used to protect hull and various areas of large vessels such as keel rudders, bottom plates, ballast tanks, bilges, cargo tanks, and structures."} img={"bg-img5"} />
+      <Hero
+        heading={"streamlined Anodes"}
+        message={
+          "Used to protect hull and various areas of large vessels such as keel rudders, bottom plates, ballast tanks, bilges, cargo tanks, and structures."
+        }
+        img={"bg-img5"}
+      />
       <div className="flex flex-col lg:flex-row items-center lg:items-start lg:justify-start pt-10 lg:py-20 w-[100%]">
         <SideBarComm />
         <article>
           <main>
-          <table>
+            <table>
               <tbody>
                 <tr>
                   <th colSpan={8} className="py-8">
