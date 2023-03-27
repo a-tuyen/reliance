d5112753a98cbc0b4d8d8b4fffb4d52e83423d62
Amanda Tuyen <atuyen26@gmail.com>
2023-03-27 09:43:55 -0700

    adds in pic and notes

--- pages/products/commercial/ballast.jsx
+++ pages/products/commercial/ballast.jsx
@@ -2,21 +2,22 @@ import React from "react";
 import Hero from "@/components/Hero";
 import SideBarComm from "@/components/SideBarComm";
 import Chart3Dim from "@/components/Chart3Dim";
+import Image from "next/image";
 
 const ballastData = [
   {
     znCode: "ZMD3602",
-    znLb: "90.0",
-    znKg: "(40.91)",
+    znLb: "90.0 lb",
+    znKg: "(40.91 kg)",
     alCode: "AMD3602",
-    alLb: "35",
-    alKg: "(16)",
-    diaIn: "60.0",
-    diaMm: "(1524)",
-    widIn: "2.5",
-    widMm: "(64)",
-    cDiaIn: "3.0",
-    cDiaMm: "(76)",
+    alLb: "35 lb",
+    alKg: "(16 kg)",
+    diaIn: '60.0"',
+    diaMm: "(1524 mm)",
+    widIn: '2.5"',
+    widMm: "(64 mm)",
+    cDiaIn: '3.0"',
+    cDiaMm: "(76 mm)",
   },
 ];
 
@@ -39,21 +40,19 @@ const ballast = () => {
                   <th colSpan={3}>NOMINAL WEIGHT</th>
                   <th colSpan={3}>NOMINAL DIMENSIONS</th>
                 </tr>
-                <tr>
-
-                </tr>
+                <tr></tr>
                 <tr>
                   <th colSpan={2}>ITEM #</th>
                   <th>
-                      <div className="flex flex-col md:flex-row justify-center items-center">
+                    <div className="flex flex-col md:flex-row justify-center items-center">
                       <div>lb</div>
                       <div className="px-2">(kg)</div>
                     </div>
                   </th>
-                  <th>DIAMETER</th>
-                  <th>WIDTH</th>
-                  <th>C-DEPTH</th>
-                  </tr>
+                  <th>A: LENGTH</th>
+                  <th>B: WIDTH</th>
+                  <th>C: DEPTH</th>
+                </tr>
                 {ballastData.map((item, id) => (
                   <Chart3Dim
                     key={id}
@@ -73,6 +72,19 @@ const ballast = () => {
                 ))}
               </tbody>
             </table>
+            <Image
+              src="https://user-images.githubusercontent.com/77664153/228003981-5a67065d-948f-4f4b-aeea-ab8a83f572fa.png"
+              alt="picture of anode"
+              className="min-w-[80px]"
+              width={200}
+              height={100}
+            />
+            <p className="pt-4 pb-4 text-center w-[80%]">
+              These Ballast Tank Anodes are available in any length up to the
+              maximum shown in the chart. Supplied with straight or bent rod
+              extensions. Other rod sizes and configurations may be specified by
+              purchaser.</p>
+            
           </main>
         </article>
       </div>
