2266fbb495f9fe17316f7d34e37093d80cdfd049
Amanda Tuyen <atuyen26@gmail.com>
2023-03-27 10:51:34 -0700

    converts heater treater to 3dim chart and adds in pic

--- pages/products/commercial/heaterTreater.jsx
+++ pages/products/commercial/heaterTreater.jsx
@@ -1,71 +1,63 @@
 import React from "react";
 import Hero from "@/components/Hero";
 import SideBarComm from "@/components/SideBarComm";
-import ChartWNote from "@/components/ChartWNote";
+import Chart3Dim from "@/components/Chart3Dim";
+import Image from "next/image";
 
 const heaterTreaterData = [
-  {
-    znCode: "TREATER Z321",
-    znLb: "39",
-    znKg: "(17.70)",
-    alCode: "-",
-    alLb: "-",
-    alKg: "-",
-    diaIn: "21.5",
-    diaMm: "(546)",
-    widIn: "3.00",
-    widMm: "(76)",
-    cDiaIn: "1.0, 1.5",
-    cDiaMm: "(25, 38)",
-    notes: "THREADED",
-  },
-  {
-    znCode: "TREATER Z321A",
-    znLb: "39",
-    znKg: "(17.70)",
-    alCode: "-",
-    alLb: "-",
-    alKg: "-",
-    diaIn: "21.5",
-    diaMm: "(546)",
-    widIn: "3.00",
-    widMm: "(76)",
-    cDiaIn: "4.0",
-    cDiaMm: "(102)",
-    notes: "THREADED",
-  },
+  {znCode: 'TREATER Z321',
+znLb: '39 lb',
+znKg: '(17.70 kg)',
+alCode: '-',
+alLb: '-',
+alKg: '',
+diaIn: '21.5"',
+diaMm: '(546 mm)',
+widIn: '3.00"',
+widMm: '(76 mm)',
+cDiaIn: '1.0", 1.5"',
+cDiaMm: '(25 mm, 38 mm)',},
 
-  {
-    znCode: "TREATER Z330",
-    znLb: "56",
-    znKg: "(25.40)",
-    alCode: "-",
-    alLb: "-",
-    alKg: "-",
-    diaIn: "30.",
-    diaMm: "(762)",
-    widIn: "300",
-    widMm: "(76)",
-    cDiaIn: "1.0, 1.5",
-    cDiaMm: "(25, 38)",
-    notes: "THREADED",
-  },
+{znCode: 'TREATER Z321A',
+znLb: '39 lb',
+znKg: '(17.70 kg)',
+alCode: '-',
+alLb: '-',
+alKg: '',
+diaIn: '21.5"',
+diaMm: '(546 mm)',
+widIn: '3.00"',
+widMm: '(76 mm)',
+cDiaIn: '4.0"',
+cDiaMm: '(102 mm)',},
 
-  {
-    znCode: "TREATER Z330A",
-    znLb: "56",
-    znKg: "(25.40)",
-    alCode: "-",
-    alLb: "-",
-    alKg: "-",
-    diaIn: "4.0",
-    diaMm: "(102)",
-    widIn: "1.25",
-    widMm: "(32)",
-    cDiaIn: "4.0",
-    cDiaMm: "(102)",
-    notes: "THREADED",
-  },
+{znCode: 'TREATER Z330',
+znLb: '56 lb',
+znKg: '(25.40 kg)',
+alCode: '-',
+alLb: '-',
+alKg: '',
+diaIn: '30."',
+diaMm: '(762 mm)',
+widIn: '300"',
+widMm: '(76 mm)',
+cDiaIn: '1.0", 1.5"',
+cDiaMm: '(25 mm, 38 mm)',},
+
+{znCode: 'TREATER Z330A',
+znLb: '56 lb',
+znKg: '(25.40 kg)',
+alCode: '-',
+alLb: '-',
+alKg: '',
+diaIn: '4.0"',
+diaMm: '(102 mm)',
+widIn: '1.25"',
+widMm: '(32 mm)',
+cDiaIn: '4.0"',
+cDiaMm: '(102 mm)',},
+
+
 ];
 
 const heaterTreater = () => {
@@ -84,39 +76,23 @@ const heaterTreater = () => {
                   </th>
                 </tr>
                 <tr>
-                  <th colSpan={6}>NOMINAL WEIGHT</th>
+                  <th colSpan={3}>NOMINAL WEIGHT</th>
                   <th colSpan={7}>NOMINAL DIMENSIONS</th>
                 </tr>
-                <tr>
-                  <th colSpan={3} className="bg-zinc">
-                    ZINC ANODE
+                 <tr>
+                  <th colSpan={2}>ITEM #</th>
+                  <th>
+                  <div className="flex flex-col md:flex-row justify-center items-center">
+                      <div>lb</div>
+                      <div className="px-2">(kg)</div>
+                    </div>
                   </th>
-                  <th colSpan={3} className="bg-alum">
-                    ALUMINUM ANODE
-                  </th>
-                  <th colSpan={2}>DIAMETER</th>
-                  <th colSpan={2}>WIDTH</th>
-                  <th colSpan={2}>C-DEPTH</th>
-                  <th>MOUNTING</th>
-                </tr>
-
-                <tr>
-                  <th className="bg-zinc">ITEM #</th>
-                  <th className="bg-zinc border-r-0">lb</th>
-                  <th className="bg-zinc border-l-0">(kg)</th>
-                  <th className="bg-alum">ITEM #</th>
-                  <th className="bg-alum border-r-0">lb</th>
-                  <th className="bg-alum border-l-0">(kg)</th>
-                  <th className="border-r-0">in</th>
-                  <th className="border-l-0">(mm)</th>
-                  <th className="border-r-0">in</th>
-                  <th className="border-l-0">(mm)</th>
-                  <th className="border-r-0">in</th>
-                  <th className="border-l-0">(mm)</th>
-                  <th></th>
+                  <th>DIAMETER</th>
+                  <th>WIDTH</th>
+                  <th>C-DEPTH</th>
                 </tr>
                 {heaterTreaterData.map((item, id) => (
-                  <ChartWNote
+                  <Chart3Dim
                     key={id}
                     znCode={item.znCode}
                     znLb={item.znLb}
@@ -136,6 +112,14 @@ const heaterTreater = () => {
               </tbody>
             </table>
           </main>
+          <Image
+                src="https://user-images.githubusercontent.com/77664153/228016568-a9612d1e-093d-49da-a19b-551c30324842.png"
+                alt="picture of anode"
+                className="min-w-[80px]"
+                width={200}
+                height={100}
+              />
+              <p className="py-4">MOUNTINGS ARE THREADED</p>
         </article>
       </div>
     </div>
