4c29586ee4343ccbc1e5308fc4053ab427a2d0d2
Amanda Tuyen <atuyen26@gmail.com>
2023-03-28 11:32:27 -0700

    add in remaining comm anodes

--- pages/products/commercial/hull.jsx
+++ pages/products/commercial/hull.jsx
@@ -304,6 +304,306 @@ const hullBarBoltData = [
   },
 ];
 
+const hullPlateWeldData = [
+  {
+    img: "https://user-images.githubusercontent.com/77664153/228328490-8bd460a8-1d57-4c5f-a419-87ee27c56a87.png",
+    znCode: "MD-Z2/ MD-ZHS-23 /MD-ZHS-10/ MD-ZMA-1126",
+    znLb: "23.0 lb",
+    znKg: "(10.43 kg)",
+    alCode: "AMD-Z2/ AMD-ZHS-23 /AMD-ZHS-10/ AMD-ZMA-1126",
+    alLb: "10.0 lb",
+    alKg: "(4.54 kg)",
+    diaIn: '12.0"',
+    diaMm: "(305 mm)",
+    widIn: '6.0"',
+    widMm: "(152 mm)",
+    cDiaIn: '1.25"',
+    cDiaMm: "(32 mm)",
+    dMeasureIn: '-"',
+    dMeasureMm: "(- mm)",
+    notes: "BOLT HOLES CAN BE ADDED ON STRAPS AS REQUIRED",
+  },
+
+  {
+    img: "https://user-images.githubusercontent.com/77664153/228328490-8bd460a8-1d57-4c5f-a419-87ee27c56a87.png",
+    znCode: "MD-ZHS-47",
+    znLb: "45 lb",
+    znKg: "(20.41 kg)",
+    alCode: "AMD-ZHS-47",
+    alLb: "26 lb",
+    alKg: "(11.79 kg)",
+    diaIn: '27.0"',
+    diaMm: "(686 mm)",
+    widIn: '5.0"',
+    widMm: "(127 mm)",
+    cDiaIn: '1.75"',
+    cDiaMm: "(44 mm)",
+    dMeasureIn: '-"',
+    dMeasureMm: "(- mm)",
+    notes: "BOLT HOLES CAN BE ADDED ON STRAPS AS REQUIRED",
+  },
+  {
+    img: "https://user-images.githubusercontent.com/77664153/228309334-2d71391f-7f41-4532-925a-1cc2a2d07012.png",
+    znCode: "MD-661",
+    znLb: "9.0 lb",
+    znKg: "(4.08 kg)",
+    alCode: "AMD-661",
+    alLb: "3.86 lb",
+    alKg: "(1 3/4 kg)",
+    diaIn: '6.0"',
+    diaMm: "(152 mm)",
+    widIn: '6.0"',
+    widMm: "(152 mm)",
+    cDiaIn: '1"',
+    cDiaMm: "(25 mm)",
+    dMeasureIn: '-"',
+    dMeasureMm: "(- mm)",
+    notes: "BOLT HOLES CAN BE ADDED ON STRAPS AS REQUIRED",
+  },
+  {
+    img: "https://user-images.githubusercontent.com/77664153/228312824-3aee9489-ddf0-44bc-b786-49cea2d9f4fc.png",
+    znCode: "MD-2431",
+    znLb: "3.08 lb",
+    znKg: "(1.4 kg)",
+    alCode: "AMD-2431",
+    alLb: "1.30 lb",
+    alKg: "(3/5 kg)",
+    diaIn: '4.0"',
+    diaMm: "(102 mm)",
+    widIn: '3.0"',
+    widMm: "(76 mm)",
+    cDiaIn: '1"',
+    cDiaMm: "(25 mm)",
+    dMeasureIn: '-"',
+    dMeasureMm: "(- mm)",
+    notes: "BOLT HOLES CAN BE ADDED ON STRAPS AS REQUIRED",
+  },
+  {
+    img: "https://user-images.githubusercontent.com/77664153/228312824-3aee9489-ddf0-44bc-b786-49cea2d9f4fc.png",
+    znCode: "MD-2631",
+    znLb: "4.50 lb",
+    znKg: "(2.04 kg)",
+    alCode: "AMD-2631",
+    alLb: "1.90 lb",
+    alKg: "(6/7 kg)",
+    diaIn: '6.0"',
+    diaMm: "(152 mm)",
+    widIn: '3.0"',
+    widMm: "(76 mm)",
+    cDiaIn: '1"',
+    cDiaMm: "(25 mm)",
+    dMeasureIn: '-"',
+    dMeasureMm: "(- mm)",
+    notes: "BOLT HOLES CAN BE ADDED ON STRAPS AS REQUIRED",
+  },
+  {
+    img: "https://user-images.githubusercontent.com/77664153/228051830-67c5c6b0-7a2d-4a93-b05a-44141b81a033.png",
+    znCode: "MD-Z-519",
+    znLb: "35.0 lb",
+    znKg: "(15.88 kg)",
+    alCode: "AMD-Z-519",
+    alLb: "13.46 lb",
+    alKg: "(6 1/9 kg)",
+    diaIn: '19.0"',
+    diaMm: "(483 mm)",
+    widIn: '5.0"',
+    widMm: "(127 mm)",
+    cDiaIn: '1 3/4"',
+    cDiaMm: "(44 mm)",
+    dMeasureIn: '-"',
+    dMeasureMm: "(- mm)",
+    notes: "BOLT HOLES CAN BE ADDED ON STRAPS AS REQUIRED",
+  },
+];
+
+const hullPlateBoltData = [
+  {
+    img: "https://user-images.githubusercontent.com/77664153/228302018-5540aba7-127d-41c1-a831-e446f233a185.png",
+    znCode: "MD-Z-1 / MD-ZHC-23/ MD-B-9/ MD-TH-24",
+    znLb: "23.0 lb",
+    znKg: "(10.43 kg)",
+    alCode: "AMD-Z-1 / AMD-ZHC-23/ AMD-B-9/ AMD-TH-24",
+    alLb: "9.7 lb",
+    alKg: "(4.4 kg)",
+    diaIn: '12.0"',
+    diaMm: "(305 mm)",
+    widIn: '6.0"',
+    widMm: "(152 mm)",
+    cDiaIn: '1.25"',
+    cDiaMm: "(32 mm)",
+    dMeasureIn: '6.0"',
+    dMeasureMm: "(152 mm)",
+    notes: "",
+  },
+
+  {
+    img: "https://user-images.githubusercontent.com/77664153/228302018-5540aba7-127d-41c1-a831-e446f233a185.png",
+    znCode: "MD-ZHC-24",
+    znLb: "23.0 lb",
+    znKg: "(10.43 kg)",
+    alCode: "AMD-ZHC-24",
+    alLb: "8.52 lb",
+    alKg: "(3.86 kg)",
+    diaIn: '12.0"',
+    diaMm: "(305 mm)",
+    widIn: '6.0"',
+    widMm: "(152 mm)",
+    cDiaIn: '1.5"',
+    cDiaMm: "(38 mm)",
+    dMeasureIn: '6.5"',
+    dMeasureMm: "(165 mm)",
+    notes: "",
+  },
+
+  {
+    img: "https://user-images.githubusercontent.com/77664153/228302018-5540aba7-127d-41c1-a831-e446f233a185.png",
+    znCode: "MD-ZHC-47",
+    znLb: "44.0 lb",
+    znKg: "(19.96 kg)",
+    alCode: "AMD-ZHC-47",
+    alLb: "19.8 lb",
+    alKg: "(8.98 kg)",
+    diaIn: '12.0"',
+    diaMm: "(305 mm)",
+    widIn: '6.0"',
+    widMm: "(152 mm)",
+    cDiaIn: '2.5"',
+    cDiaMm: "(64 mm)",
+    dMeasureIn: '6.0"',
+    dMeasureMm: "(152 mm)",
+    notes: "",
+  },
+
+  {
+    img: "https://user-images.githubusercontent.com/77664153/228302018-5540aba7-127d-41c1-a831-e446f233a185.png",
+    znCode: "MD-H20B",
+    znLb: "18.9 lb",
+    znKg: "(8.57 kg)",
+    alCode: "AMD-H20B",
+    alLb: "8.0 lb",
+    alKg: "(3.63 kg)",
+    diaIn: '12.0"',
+    diaMm: "(305 mm)",
+    widIn: '6.0"',
+    widMm: "(152 mm)",
+    cDiaIn: '1.3"',
+    cDiaMm: "(33 mm)",
+    dMeasureIn: '6.0"',
+    dMeasureMm: "(152 mm)",
+    notes: "",
+  },
+  {
+    img: "https://user-images.githubusercontent.com/77664153/228321144-cb6af4bd-c143-4dec-bb77-f485d3e87161.png",
+    znCode: "MD-Z-24B",
+    znLb: "20.0 lb",
+    znKg: "(9.07 kg)",
+    alCode: "AMD-Z-24B",
+    alLb: "9.0 lb",
+    alKg: "(4 kg)",
+    diaIn: '14.0"',
+    diaMm: "(356 mm)",
+    widIn: '6.5"',
+    widMm: "(165 mm)",
+    cDiaIn: '1 1/2"',
+    cDiaMm: "(38 mm)",
+    dMeasureIn: '5.0"',
+    dMeasureMm: "(127 mm)",
+    notes: "SLOTTED WITH ALUMINUM INSERT",
+  },
+
+  {
+    img: "https://user-images.githubusercontent.com/77664153/228066886-41ed1f10-ffa0-4bb1-924b-e2467068ef8e.png",
+    znCode: "MD-ZHC-15 HEAVY",
+    znLb: "18.0 lb",
+    znKg: "(8.16 kg)",
+    alCode: "AMD-ZHC-15 HEAVY",
+    alLb: "8.65 lb",
+    alKg: "(4 kg)",
+    diaIn: '18.0"',
+    diaMm: "(457 mm)",
+    widIn: '4.0"',
+    widMm: "(102 mm)",
+    cDiaIn: '1 3/4"',
+    cDiaMm: "(44 mm)",
+    dMeasureIn: '9"',
+    dMeasureMm: "(229 mm)",
+    notes: "",
+  },
+  {
+    img: "https://user-images.githubusercontent.com/77664153/228323266-5542f35d-e4d6-40ac-9c2c-d33960daad35.png",
+    znCode: "MD-ZHC-22",
+    znLb: "21.0 lb",
+    znKg: "(9.53 kg)",
+    alCode: "AMD-ZHC-22",
+    alLb: "7.78 lb",
+    alKg: "(3 1/2 kg)",
+    diaIn: '17.0"',
+    diaMm: "(432 mm)",
+    widIn: '4.0"',
+    widMm: "(102 mm)",
+    cDiaIn: '2"',
+    cDiaMm: "(51 mm)",
+    dMeasureIn: '8.0"',
+    dMeasureMm: "(203 mm)",
+    notes:
+      'SLOTTED WITH ALUMINUM INSERT. E - INNER DIAMETER OF 3.18" (80mm). MADE FOR "VIKING" BOATS AND MORE',
+  },
+  {
+    img: "https://user-images.githubusercontent.com/77664153/228302018-5540aba7-127d-41c1-a831-e446f233a185.png",
+    znCode: "MD-ZHC-24",
+    znLb: "23.0 lb",
+    znKg: "(10.43 kg)",
+    alCode: "AMD-ZHC-24",
+    alLb: "8.52 lb",
+    alKg: "(3 6/7 kg)",
+    diaIn: '12.0"',
+    diaMm: "(305 mm)",
+    widIn: '6.0"',
+    widMm: "(152 mm)",
+    cDiaIn: '1 1/2"',
+    cDiaMm: "(38 mm)",
+    dMeasureIn: '6.5"',
+    dMeasureMm: "(165 mm)",
+    notes: "",
+  },
+  {
+    img: "https://user-images.githubusercontent.com/77664153/228302018-5540aba7-127d-41c1-a831-e446f233a185.png",
+    znCode: "MD-ZHC-42",
+    znLb: "42.0 lb",
+    znKg: "(19.05 kg)",
+    alCode: "AMD-ZHC-42",
+    alLb: "15.5 lb",
+    alKg: "(7 kg)",
+    diaIn: '12.0"',
+    diaMm: "(305 mm)",
+    widIn: '6.0"',
+    widMm: "(152 mm)",
+    cDiaIn: '2 1/2"',
+    cDiaMm: "(64 mm)",
+    dMeasureIn: '6.0"',
+    dMeasureMm: "(152 mm)",
+    notes: 'MOUNTING HOLES ARE ROUND NOT SLOTTED. 5/8" - 6" C/C',
+  },
+  {
+    img: "https://user-images.githubusercontent.com/77664153/228302018-5540aba7-127d-41c1-a831-e446f233a185.png",
+    znCode: "MD-ZHC-44",
+    znLb: "44.0 lb",
+    znKg: "(19.96 kg)",
+    alCode: "AMD-ZHC-44",
+    alLb: "16.3 lb",
+    alKg: "(7 2/5 kg)",
+    diaIn: '12.0"',
+    diaMm: "(305 mm)",
+    widIn: '6.0"',
+    widMm: "(152 mm)",
+    cDiaIn: '2 1/2"',
+    cDiaMm: "(64 mm)",
+    dMeasureIn: '6.0"',
+    dMeasureMm: "(152 mm)",
+    notes: 'SLOTTED WITH ALUMINUM INSERT. 5/8" TO 1 1/4"',
+  },
+];
+
 const hull = () => {
   return (
     <div>
@@ -362,6 +662,58 @@ const hull = () => {
                   </th>
                 </tr>
                 {hullBarBoltData.map((item, id) => (
+                  <Chart4DimPic
+                    key={id}
+                    img={item.img}
+                    znCode={item.znCode}
+                    znLb={item.znLb}
+                    znKg={item.znKg}
+                    alCode={item.alCode}
+                    alLb={item.alLb}
+                    alKg={item.alKg}
+                    diaIn={item.diaIn}
+                    diaMm={item.diaMm}
+                    widIn={item.widIn}
+                    widMm={item.widMm}
+                    cDiaIn={item.cDiaIn}
+                    cDiaMm={item.cDiaMm}
+                    dMeasureIn={item.dMeasureIn}
+                    dMeasureMm={item.dMeasureMm}
+                    notes={item.notes}
+                  />
+                ))}
+                <tr>
+                  <th colSpan={15} className="text-center text-2xl py-1">
+                    PLATE ANODES - WELD ON
+                  </th>
+                </tr>
+                {hullPlateWeldData.map((item, id) => (
+                  <Chart4DimPic
+                    key={id}
+                    img={item.img}
+                    znCode={item.znCode}
+                    znLb={item.znLb}
+                    znKg={item.znKg}
+                    alCode={item.alCode}
+                    alLb={item.alLb}
+                    alKg={item.alKg}
+                    diaIn={item.diaIn}
+                    diaMm={item.diaMm}
+                    widIn={item.widIn}
+                    widMm={item.widMm}
+                    cDiaIn={item.cDiaIn}
+                    cDiaMm={item.cDiaMm}
+                    dMeasureIn={item.dMeasureIn}
+                    dMeasureMm={item.dMeasureMm}
+                    notes={item.notes}
+                  />
+                ))}
+                <tr>
+                  <th colSpan={15} className="text-center text-2xl py-1">
+                    PLATE ANODES - BOLT ON
+                  </th>
+                </tr>
+                {hullPlateBoltData.map((item, id) => (
                   <Chart4DimPic
                     key={id}
                     img={item.img}
