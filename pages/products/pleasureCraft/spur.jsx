d6dc7a01f790dbeef38ba4c7ceb32272aaa32d05
Amanda Tuyen <atuyen26@gmail.com>
2023-04-14 14:29:31 -0700

    adds in chart2dim to spur page

--- pages/products/pleasureCraft/spur.jsx
+++ pages/products/pleasureCraft/spur.jsx
@@ -1,34 +1,47 @@
-import React from 'react'
-import Hero from '@/components/Hero'
-import SideBarPC from '@/components/SideBarPC'
+import React from "react";
+import Hero from "@/components/Hero";
+import SideBarPC from "@/components/SideBarPC";
+import Chart2Dim from "@/components/Chart2Dim";
+
+const spurData = [];
 
 const spur = () => {
   return (
     <div>
-        <Hero
-        heading={"Spurs Line Cutter Anodes"}
-        message={""}
-        img={"bg-img2"}
-        />
-        <div className="flex flex-col lg:flex-row items-center lg:items-start lg:justify-start pt-10 lg:py-20 w-[100%]">
-            <SideBarPC />
-            <article>
-                <main>
-                    <table>
-                        <tbody>
-                            <tr>
-                                <th>
-
-                                </th>
-                            </tr>
-                        </tbody>
-                    </table>
-                </main>
-            </article>
-        </div>
-
+      <Hero heading={"Spurs Line Cutter Anodes"} message={""} img={"bg-img2"} />
+      <div className="flex flex-col lg:flex-row items-center lg:items-start lg:justify-start pt-10 lg:py-20 w-[100%]">
+        <SideBarPC />
+        <article>
+          <main>
+            <h1>SPURS LINE CUTTER ANODE SPECIFICATIONS</h1>
+            <table>
+              <tbody>
+                <tr>
+                  <th>NOMINAL WEIGHT</th>
+                  <th>NOMINAL DIMENSIONS</th>
+                </tr>
+                {spurData.map((item, id) => (
+                  <Chart2Dim
+                    key={id}
+                    znCode={item.znCode}
+                    znLb={item.znLb}
+                    znKg={item.znKg}
+                    alCode={item.alCode}
+                    alLb={item.alLb}
+                    alKg={item.alKg}
+                    diaIn={item.diaIn}
+                    diaMm={item.diaMm}
+                    widIn={item.widIn}
+                    widMm={item.widMm}
+                  />
+                ))}
+              </tbody>
+            </table>
+          </main>
+        </article>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default spur
+export default spur;
