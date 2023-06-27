f14d1e66e231720a9104560ab5d19a802357bde7
Amanda Tuyen <atuyen26@gmail.com>
2023-06-27 14:42:12 -0700

    makes banner visib;e/hidden on scroll

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,12 +1,26 @@
-
+import { useState, useEffect } from "react";
 
 const Banner = () => {
+  const [bannerHidden, setBannerHidden] = useState(true);
+
+  useEffect(() => {
+    const changeDisplay = () => {
+      if (window.scrollY >= 90) {
+        setBannerHidden(false);
+      } else {
+        setBannerHidden(true);
+      }
+    };
+    window.addEventListener("scroll", changeDisplay);
+  }, []);
 
   return (
     <>
-<div className="fixed top-[120px] left-[0%] w-[100%] z-50 text-center">
-  <h1 className="text-center font-banner text-white text-lg tracking-wider bg-black py-2">Zinc for salt water. Aluminum for salt and brackish water</h1>
-  </div>
+      <div className={bannerHidden ? "hidden" : "fixed top-[120px] left-[0%] w-[100%] z-50 text-center"}>
+        <h1 className="text-center font-banner text-white text-lg tracking-wider bg-black py-1">
+          Zinc for salt water. Aluminum for salt and brackish water
+        </h1>
+      </div>
     </>
   );
 };
