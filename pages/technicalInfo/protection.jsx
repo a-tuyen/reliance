9d67bb9fe2aced8b3c2ed1ee2ddde4769b7c34ea
Amanda Tuyen <atuyen26@gmail.com>
2024-11-28 14:48:59 -0800

    editting grammar

--- pages/technicalInfo/protection.jsx
+++ pages/technicalInfo/protection.jsx
@@ -8,15 +8,15 @@ const milSpec = () => {
         <Hero heading={"Boat Protection Basics"} img={"bg-img3"} />
       </div>
       <main>
-        <article className="bg-white flex flex-col justify-center items-center">
+        <article className="bg-white flex flex-col justify-center items-center mt-4">
           <h2>Considerations for Boat Protection</h2>
           <div>
-            <h4 className="flex justify-center">GALVANIC CORROSION</h4>
+            <h4 className="flex justify-center mt-4">GALVANIC CORROSION</h4>
             <p className='mb-4'>
               When two or more dissimiliar metals are in contact with each other and immersed in an electroyte (a liquid that can conduct electricity), the more electrically active metal (less noble) will sacrifice itself by electron flow from the more negatively charged metal (the anode) to the more positively charged metal (the cathode).  Hence, for protection of metal fittings on boats as well as hulls and drive units, a comprehensive plan of protection needs to be employed.
             </p>
 
-            <p className='mb-4'>By utilizing sacrificial anodes, these components will be protected because they are not corroding themselves with the loss of material as is the anode.  For example, on a steel hulled boat with brass fittings submerged underwater, the two metals provide the anode (steel) and cathode (brass).  The water completes the circuit just like a battery.  In this arrangement, the steel will deteriorate as it is less noble than the brass.  To protect both metals, a third metal is introduced that is less noble than the other two.  The metals widely used for this cathodic production are zinc, aluminum and magnesium.  Each element has characteristics that make them suitable for certain applications.</p>
+            <p className='mb-4'>By utilizing sacrificial anodes, these components will be protected because they are not corroding themselves with the loss of material, instead the anode is.  For example, on a steel hulled boat with brass fittings submerged underwater, the two metals provide the anode (steel) and cathode (brass).  The water completes the circuit just like a battery.  In this arrangement, the steel will deteriorate as it is less noble than the brass.  To protect both metals, a third metal is introduced that is less noble than the other two.  The metals widely used for this cathodic production are zinc, aluminum and magnesium.  Each element has characteristics that make them suitable for certain applications.</p>
             <p className='mb-4'>Factors that affect galvanic corrosion are the salinity of water, the pollutants present, the water flow rate, cavitation, oxygen content, temperature, etc.
             </p>
             <p className='mb-4'>
@@ -98,9 +98,9 @@ const milSpec = () => {
             <p className='flex justify-center mt-1 mb-4'>( -1050 Zinc) - (-450 Brass) = -600 mV or -0.6 V.</p>
           </div>
 
-          <p className='mb-6'>A general rule of thumb for cathodic protection is to provide a negative voltage that is at a minimum of -0.2V(-200mV) relative to the least noble metal being protected.  There is, however, the posiblity of over protection as well as under protection in certain situations.
+          <p className='mb-6'>A general rule of thumb for cathodic protection is to provide a negative voltage that is at a minimum of -0.2V(-200mV) relative to the least noble metal being protected.  There is, however, the possiblity of overprotection as well as under-protection in certain situations.
 
-            Over protection can cause damage in the form of alkali corrosion to aluminum, delignification to wooden hull (the breakdown of the fibres), and hull coatings (blistering).  This can occur, for example, if a magnesium anode is used on aluminum, in water that is polluted, brackish (in between fresh and seawater) or seawater resulting in too much voltage. Another cause is stray DC current from defective wiring or equipment.</p> 
+            Overprotection can cause damage in the form of alkali corrosion to aluminum, delignification to wooden hull (the breakdown of the fibres), and hull coatings (blistering).  This can occur, for example, if a magnesium anode is used on aluminum, in water that is polluted, brackish (in between fresh and seawater) or seawater resulting in too much voltage. Another cause is stray DC current from defective wiring or equipment.</p> 
 
           <h4>APPROXIMATE RECOMMENDED RANGE OF CATHODIC PROTECTION</h4>
           <ul className='mb-4'>
@@ -112,7 +112,7 @@ const milSpec = () => {
           </ul>
 
           <p className='mb-4'>
-            The ability to attain the -200 mv negative shift is dependent on the amount of current (amperage) the anode generates in relationship to the area of coverage.  This is determined by the anodes surface area and proximity to the metal being protected.
+            The ability to attain the -200 mv negative shift is dependent on the amount of current (amperage) the anode generates in relationship to the area of coverage.  This is determined by the anode&apos;s surface area and proximity to the metal being protected.
 
             Factors that cause current requirements to be higher are exposed areas (uncoated), water speed, and water temperature.  Pure zinc has a theoretical capacity of 372 Ampere Hours per pound.  That means at 1 amp, it would take 372 hours to consume a pound of zinc.  Now, zinc operates at about 95% efficiency so it would actually be approximately 353 Ampere Hours.
 
