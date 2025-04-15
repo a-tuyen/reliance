42afc9fb721cb4a67dd9f9101097e0b5045972a4
Amanda Tuyen <atuyen26@gmail.com>
2025-04-15 09:25:21 -0700

    adjusts formatting of about page

--- pages/about.jsx
+++ pages/about.jsx
@@ -4,19 +4,21 @@ import Image from "next/image";
 
 const about = () => {
   return (
-    <div className="bg-white flex flex-col">
+    <div className="bg-white w-[100%]"> 
+
       <Hero heading={"About Us"} img={"bg-img3"} />
       
-      <article className='flex lg:flex-row items-center justify-center ml-[10%] mt-[10%] lg:mt-[1%]'>
-        <div className="text-xl w-[90%] lg:w-[50%] lg:text-3xl tracking-widest font-thin flex flex-col">
-          <h3 className='pb-4'>A Legacy Cast in Metal</h3>
+      <article className='flex flex-col lg:flex-row justify-center items-center w-[100%]'>
+        <div className='flex flex-col items-center lg:flex-row justify-center w-[100%]'>
+        <div className="w-[90%] lg:w-[50%] lg:text-3xl tracking-wide font-thin flex flex-col">
+          <h2 className='p-10 lg:pb-12'>A Legacy Cast in Metal</h2>
           <p>
           Reliance Anodes was founded in 1988 by Bob, continuing a family tradition that began in 1949. Three generations later, we’re still hands-on, quality-focused, and proudly manufacturing under conditions that prioritize people, process, and precision.
           </p>
-          <p className="pt-4">
+          <p className="py-4 lg:py-8">
           We don’t just make anodes—we make them right. It’s a product of experience, of high standards, and of a belief that honest work should lead to something that lasts.
           </p>
-          <p className="pt-4 italic">
+          <p className="pb-12 lg:pb-20 italic">
           For us, it’s not just corrosion protection. It’s a piece of the Canadian dream—realized through craftsmanship, care, and doing the job right.
           </p>
         </div>
@@ -24,9 +26,11 @@ const about = () => {
         src="/about/anode-bot.jpg"
         width="300"
         height="300"
-        className="mx-[10vw] my-[10vw] lg:mx-[5vw] lg:my-[10vw] rounded-xl"
+        className="mx-[10vw] lg:mx-[5vw] my-[10vw] lg:my-[10vw] rounded-xl"
       />
+        </div>
       </article>
+    
 
     </div>
   )
